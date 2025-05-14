572a26a64db6b0748f322e89cb503da69a6ead5a
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-05-14 22:13:18 +0300

    created inheritance to share behaviour from super class

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,12 @@ from user import User
 import random
 
 class Teacher(User):
-
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Math", "Science", "History"] #teacher's knowledge   
+        #self.knowledge = random.choice(self.knowledge)
+    
     def teach(self):
-        pass
+        return self.knowledge[0]
+        #return self.knowledge[random.randint(0, len(self.knowledge)-1)]
+        
